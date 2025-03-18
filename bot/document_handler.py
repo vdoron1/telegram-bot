9a518a0efa5d884c68b65722d645016945a36f61
vdoron1 <vdoron05@gmail.com>
2025-03-18 21:52:39 +0300

    Sourse Control

--- bot/document_handler.py
+++ bot/document_handler.py
@@ -3,7 +3,9 @@ import json
 import docx
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 from langchain_community.vectorstores import FAISS
 
 # Пути к данным
